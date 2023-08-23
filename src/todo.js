530bd5446c04f9b827959885eb3a963039b7a4fd
s-jad <s_jad@web.de>
2023-08-23 15:27:06 +0200

    Refactor: removed todo checklists

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,10 @@
 export default class Todo {
-    constructor(title, description, dueDate, priority, notes, checklist) {
+    constructor(title, description, dueDate, priority, notes) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
-        this.checklist = checklist;
     };
 }
 
