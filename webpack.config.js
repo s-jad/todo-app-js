08065b9833812ed80612ddfd63c349d0fa57114b
s-jad <s_jad@web.de>
2023-09-11 13:06:07 +0200

    New package.json/package-lock.json/webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',
